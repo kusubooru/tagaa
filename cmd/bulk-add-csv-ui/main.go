3389999346028613c11bbe83b9e21d3ca174c511
Kusubooru <kusubooru@gmail.com>
2015-12-30 09:53:15 +0200

    Add model, serveImage and submit form

--- cmd/bulk-add-csv-ui/main.go
+++ cmd/bulk-add-csv-ui/main.go
@@ -8,25 +8,44 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"runtime"
+	"strings"
 
 	"github.com/kusubooru/shimmie2-tools/bulk"
 )
 
-var templates = template.Must(template.ParseGlob("web/*.tmpl"))
+var fns = template.FuncMap{
+	"last": func(s []string) string {
+		if len(s) == 0 {
+			return ""
+		}
+		return s[len(s)-1]
+	},
+}
+
+var templates = template.Must(template.New("").Funcs(fns).ParseGlob("web/*.tmpl"))
 
 var (
 	directory   = flag.String("d", "", "the directory that contains the images")
 	csvFilename = flag.String("csv", "bulk.csv", "the name of the CSV file")
-	pathPrefix  = flag.String("prefix", "", "the path that should be prefixed before the directory and the image name on the CSV file.")
+	pathPrefix  = flag.String("prefix", "", "the path that should be prefixed before the directory and the image name on the CSV file")
 	port        = flag.String("port", "8080", "server port")
 )
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "Usage: bulk-add-csv-ui -d <directory with images> [options...]\n")
+	fmt.Fprintf(os.Stderr, "Usage: bulk-add-csv-ui -d <directory with images> [Options...]\n")
 	fmt.Fprintf(os.Stderr, "Options:\n")
 	flag.PrintDefaults()
 }
+
+type Model struct {
+	Prefix string
+	Dir    string
+	Images []bulk.Image
+}
+
+var model = Model{}
 
 func main() {
 	if err := run(); err != nil {
@@ -38,23 +57,31 @@ func main() {
 func run() error {
 	flag.Usage = usage
 	flag.Parse()
+
 	if *directory == "" {
 		usage()
 		return fmt.Errorf("argument -d is required")
 	}
+	model.Dir = *directory
+
 	images, err := bulk.Load(*directory, *csvFilename)
 	if err != nil {
 		return err
 	}
-	fmt.Fprintf(ioutil.Discard, "", images)
+	model.Images = images
 
-	prefix, err := bulk.CurrentPrefix(*directory, *csvFilename)
+	currentPrefix, err := bulk.CurrentPrefix(*directory, *csvFilename)
 	if err != nil {
 		return err
 	}
-	fmt.Fprintf(ioutil.Discard, prefix)
+	if *pathPrefix != "" && *pathPrefix != currentPrefix {
+		return fmt.Errorf("path prefix conflict: -prefix is %v, while csv file is %v", *pathPrefix, currentPrefix)
+	}
+	model.Prefix = currentPrefix
 
 	http.Handle("/", http.HandlerFunc(index))
+	http.Handle("/update", http.HandlerFunc(update))
+	http.Handle("/img/", http.HandlerFunc(serveImage))
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static"))))
 
 	fmt.Println("Starting server at :" + *port)
@@ -68,14 +95,53 @@ func run() error {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	render(w, "index", nil)
-	//w.Write([]byte(staticHtml))
+	render(w, "index", model)
 }
 
 func render(w http.ResponseWriter, tmpl string, model interface{}) {
 	if err := templates.ExecuteTemplate(w, tmpl+".tmpl", model); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func update(w http.ResponseWriter, r *http.Request) {
+
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, "could not parse form", http.StatusInternalServerError)
+	}
+
+	// prefix
+	model.Prefix = r.PostForm["prefix"][0]
+	for _, img := range model.Images {
+		// tags
+		areaTags := r.PostForm[fmt.Sprintf("image[%d].tags", img.ID)]
+		model.Images[img.ID].Tags = strings.Fields(areaTags[0])
+		// source
+		model.Images[img.ID].Source = r.PostForm[fmt.Sprintf("image[%d].source", img.ID)][0]
+		// rating
+		rating := r.PostForm[fmt.Sprintf("image[%d].rating", img.ID)]
+		if len(rating) != 0 {
+			model.Images[img.ID].Rating = rating[0]
+		}
+	}
+
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func serveImage(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[strings.LastIndex(r.URL.Path, "/")+1:]
+	img := bulk.FindByID(model.Images, id)
+	p := filepath.Join(*directory, img.Name)
+
+	f, err := os.Open(p)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("could not open image %v", p), http.StatusInternalServerError)
+	}
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("could not read image %v", p), http.StatusInternalServerError)
+	}
+	w.Write(data)
 }
 
 func browserOpen(input string) error {
