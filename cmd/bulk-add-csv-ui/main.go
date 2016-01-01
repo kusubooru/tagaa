e8064fc17c05ef903486e7f3363bd0a4955f71c2
Kusubooru <kusubooru@gmail.com>
2016-01-02 00:27:44 +0200

    Add browserOpen before starting the server

--- cmd/bulk-add-csv-ui/main.go
+++ cmd/bulk-add-csv-ui/main.go
@@ -86,6 +86,11 @@ func run() error {
 	http.Handle("/img/", http.HandlerFunc(serveImage))
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static"))))
 
+	localURL := fmt.Sprintf("http://localhost:%v", *port)
+	if err := browserOpen(localURL); err != nil {
+		fmt.Sprintln(os.Stderr, "could not open browser, please visit %v manually", localURL)
+	}
+
 	fmt.Println("Starting server at :" + *port)
 	if err := http.ListenAndServe(":"+*port, nil); err != nil {
 		return err
