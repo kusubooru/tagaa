bb2f31cb2fac272dad6c28b2eb0ec5acb589d657
Kusubooru <kusubooru@gmail.com>
2017-05-24 23:16:15 +0300

    Add experimental upload server with limiter

--- uploadserver.go
+++ uploadserver.go
@@ -10,7 +10,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-	"strings"
+	"sync"
 	"time"
 )
 
@@ -25,8 +25,115 @@ var (
 	uploadDir = flag.String("updir", "tagaa_uploads", "upload directory")
 )
 
+type UploadsLimiter interface {
+	Reset() error
+	CanUpload(int64) (bool, error)
+	//AddSpace()
+}
+
+type limiter struct {
+	maxSpace    int64
+	addAmount   int64
+	addInterval time.Duration
+	addTicker   *time.Ticker
+	mu          sync.Mutex
+	spaceLeft   int64
+	lastAdd     time.Time
+}
+
+func NewUploadsLimiter(maxSpace, addAmount int64, addInterval time.Duration) UploadsLimiter {
+	lim := &limiter{
+		maxSpace: maxSpace,
+		//spaceLeft: maxSpace,
+		addAmount: addAmount,
+		addTicker: time.NewTicker(addInterval),
+	}
+
+	go func() {
+		for range lim.addTicker.C {
+			lim.addSpace()
+		}
+	}()
+	return lim
+}
+
+func (lim *limiter) addSpace() {
+	lim.mu.Lock()
+	defer lim.mu.Unlock()
+
+	lim.lastAdd = time.Now()
+	if lim.spaceLeft+lim.addAmount > lim.maxSpace {
+		lim.spaceLeft = lim.maxSpace
+	} else {
+		prevSpace := lim.spaceLeft
+		lim.spaceLeft += lim.addAmount
+		fmt.Printf("space: %d, add: %d, new space: %d\n", prevSpace, lim.addAmount, lim.spaceLeft)
+	}
+}
+
+//func (lim *limiter) tryAgainIn(n int64) time.Duration {
+//timeSinceLastAdd := time.Now().Sub(lim.lastAdd)
+//time.Now().Sub(lim.addInterval).Sub(lim.lastAdd)
+//lim.addInterval.Hours()
+//intervals := n / lim.addAmount
+//return intervals * lim.addInverval
+
+//   n := 100
+//   addAmount := 5
+//   intervals := n / addAmount
+//   //againTime := time.Now()
+//   //for i := 0; i < intervals; i++ {
+//   //	againTime = againTime.Add(addInterval)
+//   //}
+//   againIn := time.Second * intervals
+//   //fmt.Println("try again in", againTime.Sub(time.Now()))
+//   fmt.Println(againIn)
+//}
+
+func (lim *limiter) CanUpload(n int64) (bool, error) {
+	lim.mu.Lock()
+	defer lim.mu.Unlock()
+
+	if lim.spaceLeft == 0 || lim.spaceLeft < n {
+		return false, nil
+	}
+
+	lim.spaceLeft -= n
+	if lim.spaceLeft < 0 {
+		lim.spaceLeft = 0
+	}
+
+	return true, nil
+}
+
+func (lim *limiter) Reset() error {
+	lim.mu.Lock()
+	defer lim.mu.Unlock()
+
+	lim.spaceLeft = lim.maxSpace
+	return nil
+}
+
+type app struct {
+	limiter UploadsLimiter
+}
+
 func main() {
-	http.HandleFunc("/upload", upload)
+	flag.Parse()
+
+	lim := NewUploadsLimiter(100<<20, 10<<20, time.Second*1)
+	fmt.Println(lim)
+	//addTicker := time.NewTicker(time.Second * 1)
+
+	//go func() {
+	//	for range addTicker.C {
+	//		lim.AddSpace()
+	//	}
+	//}()
+
+	app := app{lim}
+
+	http.HandleFunc("/upload", app.upload)
 
 	mkDirIfNotExist(*uploadDir, 0700)
 
@@ -46,13 +153,19 @@ func mkDirIfNotExist(name string, perm os.FileMode) error {
 	return nil
 }
 
-func upload(w http.ResponseWriter, r *http.Request) {
-	r.Body = http.MaxBytesReader(w, r.Body, maxFileSize)
+func (app *app) upload(w http.ResponseWriter, r *http.Request) {
+	log.Println("received upload")
 
+	// Limit max upload.
+	r.Body = http.MaxBytesReader(w, r.Body, maxFileSize)
 	if err := r.ParseMultipartForm(defaultMaxMemory); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	username := r.PostFormValue("username")
+	//_ := r.PostFormValue("password")
+	// handle authentication
 
 	file, handler, err := r.FormFile(uploadFormFileName)
 	if err != nil {
@@ -61,32 +174,40 @@ func upload(w http.ResponseWriter, r *http.Request) {
 	}
 	defer file.Close()
 
-	ip := GetOriginalIP(r)
-	uploadLocation := filepath.Join(*uploadDir, time.Now().Format("2006-01-02"), ip)
+	uploadLocation := filepath.Join(*uploadDir, username)
 	if err = os.MkdirAll(uploadLocation, 0700); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	f, err := os.Create(filepath.Join(uploadLocation, filepath.Base(handler.Filename)))
+	filename := time.Now().Format("2006-01-02_15.04.05.000_") + filepath.Base(handler.Filename)
+	f, err := os.Create(filepath.Join(uploadLocation, filename))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer f.Close()
 
-	fmt.Fprintf(w, "%v", handler.Header)
-	io.Copy(f, file)
-}
-
-// GetOriginalIP gets the original IP of the HTTP for the case of being behind
-// a proxy. It searches for the X-Forwarded-For header.
-func GetOriginalIP(r *http.Request) string {
-	x := r.Header.Get("X-Forwarded-For")
-	if x != "" && strings.Contains(r.RemoteAddr, "127.0.0.1") {
-		// format is comma separated
-		return strings.Split(x, ",")[0]
+	n, err := io.Copy(f, file)
+	if err != nil {
+		http.Error(w, "file copy failed", http.StatusInternalServerError)
+		log.Println("file copy failed:", err)
+		return
 	}
-	// it also contains the port
-	return strings.Split(r.RemoteAddr, ":")[0]
+	fmt.Println("wrote:", n)
+
+	// TODO: ask limiter service for space
+	ok, err := app.limiter.CanUpload(n)
+	if err != nil || !ok {
+		log.Println("add to limiter failed")
+		if rerr := os.Remove(f.Name()); rerr != nil {
+			log.Println("file cleanup failed:", rerr)
+			http.Error(w, "file cleanup failed", http.StatusInternalServerError)
+			return
+		}
+		http.Error(w, "add to limiter failed", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "%v", handler.Header)
+	// TODO: send mail
 }
