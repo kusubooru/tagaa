kusubooru/tagaa

commit fa334fa1424405c859fa3d0dea53a369b08345d3

Exit program when browser window closes, fix #7

templates.go
@@ -252,6 +252,13 @@ var (
     (function(){
       "use strict";
 
+      // Send POST /exit when window closes.
+      window.onbeforeunload = function (event) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "exit", true);
+        xhr.send();
+      };
+
       var csvButtons = document.getElementsByClassName("save-to-csv");
       for(var i=0; i < csvButtons.length; i++) {
           csvButtons[i].onclick = setScroll;

web/index.tmpl
@@ -153,6 +153,13 @@
     (function(){
       "use strict";
 
+      // Send POST /exit when window closes.
+      window.onbeforeunload = function (event) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "exit", true);
+        xhr.send();
+      };
+
       var csvButtons = document.getElementsByClassName("save-to-csv");
       for(var i=0; i < csvButtons.length; i++) {
           csvButtons[i].onclick = setScroll;

main.go
@@ -54,6 +54,7 @@ var (
 	openBrowser = flag.Bool("openbrowser", true, "open browser automatically")
 	version     = flag.Bool("v", false, "print program version")
 	uploadURL   = flag.String("uploadurl", "https://kusubooru.com/suggest/upload", "URL to upload zip file")
+	noexit      = flag.Bool("noexit", false, "if set to true the program will keep running even if the browser window closes")
 )
 
 const description = `
@@ -152,6 +153,7 @@ func run() error {
 	http.Handle("/upload", http.HandlerFunc(uploadHandler))
 	http.Handle("/tags", http.HandlerFunc(tagsHandler))
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static"))))
+	http.Handle("/exit", http.HandlerFunc(exitHandler))
 
 	go func() {
 		localURL := fmt.Sprintf("http://localhost:%v", *port)
@@ -401,6 +403,15 @@ func serveImage(w http.ResponseWriter, r *http.Request) {
 	if _, err = w.Write(data); err != nil {
 		http.Error(w, fmt.Sprintf("could not write image bytes: %v", err), http.StatusInternalServerError)
 		return
+	}
+}
+
+func exitHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+	if !*noexit {
+		os.Exit(0)
 	}
 }
 

commit 3685f7b42719c8069e1761d31e4610343f5f01b8

Exit program only when mouse is out of the window
Commit fa334fa introduced a bug where the program exits even when trying
to save to CSV. This change adds detection for mouse out. On mouse out
the exit handler gets enabled and disabled otherwise.

This change is for issue #7.

templates.go
@@ -252,8 +252,15 @@ var (
     (function(){
       "use strict";
 
-      // Send POST /exit when window closes.
-      window.onbeforeunload = function (event) {
+      // When mouse leaves the window, the exit handler gets enabled.
+      window.onmouseout = function() {
+        window.onbeforeunload = exitHandler;
+      }
+      window.onmouseover = function() {
+        window.onbeforeunload = null;
+      }
+
+      function exitHandler(event) {
         var xhr = new XMLHttpRequest();
         xhr.open("POST", "exit", true);
         xhr.send();

web/index.tmpl
@@ -153,8 +153,15 @@
     (function(){
       "use strict";
 
-      // Send POST /exit when window closes.
-      window.onbeforeunload = function (event) {
+      // When mouse leaves the window, the exit handler gets enabled.
+      window.onmouseout = function() {
+        window.onbeforeunload = exitHandler;
+      }
+      window.onmouseover = function() {
+        window.onbeforeunload = null;
+      }
+
+      function exitHandler(event) {
         var xhr = new XMLHttpRequest();
         xhr.open("POST", "exit", true);
         xhr.send();